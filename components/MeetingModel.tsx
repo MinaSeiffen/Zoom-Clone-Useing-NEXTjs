716317d5b9ff63a02bec6c2241e9a0fcd201f2ad
MinaE.Seiffen <minaseiffen@gmail.com>
2024-05-06 22:12:43 +0300

    Whole Project

---
+++ components/MeetingModel.tsx
@@ -0,0 +1,65 @@
+import React, { ReactNode } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+} from "@/components/ui/dialog";
+
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+interface MeetingModelProps {
+  onClose: () => void;
+  title: string;
+  btnText?: string;
+  className?: string;
+  isOpen: boolean;
+  handleClick?: () => void;
+  children?: ReactNode;
+  image?: string;
+  btnIcon?: string;
+}
+
+const MeetingModel = ({
+  isOpen,
+  onClose,
+  btnText,
+  title,
+  className,
+  handleClick,
+  children,
+  image,
+  btnIcon,
+}: MeetingModelProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="image" width={72} height={72} />
+            </div>
+          )}
+          <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {" "}
+            {title}{" "}
+          </h1>
+          {children}
+          <Button
+            className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+            onClick={handleClick}
+          >
+            {btnIcon && (
+              <Image src={btnIcon} alt="BTN icon" width={13} height={13} />
+            )}{" "}
+            &nbsp;
+            {btnText || "Schedule Meeting"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MeetingModel;
